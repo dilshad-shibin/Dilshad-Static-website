763674b3bd141986a6aad9dd15cbd6855b6f6a29
dilshad-shibin <msdcva786@gmail.com>
2021-12-17 23:57:10 +0530

    mail file modified

--- mail.php
+++ mail.php
@@ -3,15 +3,11 @@
 
 $name = $_POST['name'];
 $email= $_POST['email'];
-$number= $_POST['phone'];
 $message= $_POST['message'];
 $to = "dilshadshibinweb05@gmail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name 
-. "\r\n  Email = " . $email 
-."\r\n  Phone = " . $number 
-."\r\n Message =" . $message;
-$headers = "From: noreply@dilshadshibin.com" . "\r\n" .
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
