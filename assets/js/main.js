b93eb9bf4a1a11e5c7dd7c509fae4c8b4c03256d
dilshad-viralmafia <dilshad@theviralmafia.com>
2023-04-06 15:39:28 +0530

    web all sections changed

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+    'use strict';
+
+    var $window = $(window);
+    var zero = 0;
+
+   
+   
+
+    // :: 7.0 AOS ACTIVE CODE
+    AOS.init();
+
+    // :: 8.0 PREVENT DEFAULT ACTIVE CODE
+    $("a[href='#']").on('click', function ($) {
+        $.preventDefault();
+    });
+
+   
+
+
+   
+
+}(jQuery));
